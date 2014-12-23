4ebdf495725db5c808d78b799bd7d00e43fa54b5
Jake Solomon <jacob@codeforamerica.org>
2014-12-23 09:22:58 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StayConnected::Application.config.secret_key_base = 'd8b8ee7d91b37eb1c1c838900c8d8f896a958d962d6a68ad774df5d71da9feea45fefb14446b7dbdc258b55f9818492cb0e902990740f3ddf68a5f273f6817e5'
